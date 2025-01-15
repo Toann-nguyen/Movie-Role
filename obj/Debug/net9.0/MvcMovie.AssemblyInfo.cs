75aef481906fd28fa81e3f456246e675db8b3947
Toan Nguyen <nguyenminhtoan2712py@gmail.com>
2025-01-15 17:39:37 +0700

    push after using Vnpay

--- obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
+++ obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91f7be51b1eda8f49864ff0e77b9a136b270fea3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31a8e5ba80c396788632f04dfd13407a6fcbc661")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
